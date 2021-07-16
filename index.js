9a15d2262230e5d5ac30d71410638697d8f4844d
Sajid Rahman <potatorahman@gmail.com>
2021-07-16 02:45:24 -0400

    got mock db testing to work

--- index.js
+++ index.js
@@ -1,36 +1,15 @@
 // Pull packages
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const { errorHandler } = require('./middleware/ErrorHandler');
 require('dotenv').config();
+require('./mongoConfig');
 
 const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
-
-const uri = process.env.DATABASE;
-mongoose.connect(uri, 
-  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
-);
-
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
-})
-connection.on('reconnected', () => {
-  console.log('Mongo has reconnected')
-})
-connection.on('error', error => {
-  console.log('Mongo connection has an error', error)
-  mongoose.disconnect()
-})
-connection.on('disconnected', () => {
-  console.log('Mongo connection is disconnected')
-})
-
 
 // for CRUD/REST api endpoints
 const userRouter = require('./routes/user');
--- test/user.js
+++ test/user.js
@@ -1,46 +1,83 @@
-//During the test the env variable is set to test
-process.env.NODE_ENV = 'test';
+const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
 
-let mongoose = require("mongoose");
-
-var userMethods = require("../controllers/methods/user");
-var ApiError = require("../middleware/ErrorTypes");
+let userMethods = require("../controllers/methods/user");
+let ApiError = require("../middleware/ErrorTypes");
 
 //Require the dev-dependencies
-let chai = require('chai');
+let chai = require("chai");
+let chaiAsPromised = require("chai-as-promised");
+ 
+chai.use(chaiAsPromised);
+ 
+
 let chaiHttp = require('chai-http');
 let server = require('../index');
+// Then either:
+let expect = chai.expect;
+// or:
+let assert = chai.assert;
 let should = chai.should();
 
+let mongoServer;
+const opts = { useNewUrlParser: true, dbName: "verifyMASTER", useCreateIndex: true, useUnifiedTopology: true };
 
 chai.use(chaiHttp);
 //Our parent block
 describe('User', () => {
 
+  before(async () => {
+    mongoServer = await MongoMemoryServer.create();
+    const mongoUri = mongoServer.getUri();
+    // disconnect from real server running and use mock server instead...idk how to do this better
+    await mongoose.disconnect();
+    await mongoose.connect(mongoUri, opts);
+  });
+  
+  after(async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+  });
+
 /*
   * Test the /GET route
   */
   describe('/POST register/login', () => {
-      it('should register success', (done) => {
-        chai.request(server)
-            .post('/user/register')
-            .send(testUser)
-            .end((err, res) => {
-              res.should.have.status(200);
-              done();
-            });
-      });
 
-      it('shouldnt register cuz duplicate', (done) => {
-        chai.request(server)
-            .post('/user/register')
-            .send(testUser)
-            .end((err, res) => {
-              res.should.have.status(400);
-              res.body.should.have.property('message').eql(ApiError.UserExistsError.message);
-              done();
-            })
-      });
+    it('should register success', async () => {
+      const { username, password } = testUser;
+      const r = await userMethods.register(username, password);
+      r.should.have.property('success').eql(true);
+    });
+
+    it('shouldnt register cuz duplicate', async () => {
+      userMethods.register(testUser.username, testUser.password).should.eventually.throw(ApiError.UserExistsError);
+    });
+
+    it('login failiure', async () => {
+      userMethods.login(wrongpass.username, wrongpass.password).should.eventually.throw(ApiError.LoginFailError);
+    });
+
+      // it('should register success', (done) => {
+      //   chai.request(server)
+      //       .post('/user/register')
+      //       .send(testUser)
+      //       .end((err, res) => {
+      //         res.should.have.status(200);
+      //         done();
+      //       });
+      // });
+
+      // it('shouldnt register cuz duplicate', (done) => {
+      //   chai.request(server)
+      //       .post('/user/register')
+      //       .send(testUser)
+      //       .end((err, res) => {
+      //         res.should.have.status(400);
+      //         res.body.should.have.property('message').eql(ApiError.UserExistsError.message);
+      //         done();
+      //       })
+      // });
 
       it('should be able to login', (done) => {
         chai.request(server)
@@ -52,16 +89,16 @@ describe('User', () => {
             });
       });
 
-      it('wrong login', (done) => {
-        chai.request(server)
-            .post('/user/login')
-            .send(wrongpass)
-            .end((err, res) => {
-              res.should.have.status(400);
-              res.body.should.have.property('message').eql(ApiError.LoginFailError.message);
-              done();
-            });
-      });
+      // it('wrong login', (done) => {
+      //   chai.request(server)
+      //       .post('/user/login')
+      //       .send(wrongpass)
+      //       .end((err, res) => {
+      //         res.should.have.status(400);
+      //         res.body.should.have.property('message').eql(ApiError.LoginFailError.message);
+      //         done();
+      //       });
+      // });
   });
 
 });
